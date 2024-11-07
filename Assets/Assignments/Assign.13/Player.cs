b39ecf616cc385da9413475c617ad120d6cea3b3
Mariam AL-batool <mariamalbatoolfarrah@gmail.com>
2024-11-07 18:47:07 +0200

    Finish the player class

--- Assets/Assignments/Assign.13/Player.cs
+++ Assets/Assignments/Assign.13/Player.cs
@@ -2,7 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player 
+public class Player
 {
-   
+    string playerName;
+    int haelth;
+    public static int playerCount = 0;
+    public void InitializePlayer(string name, int initialHealth)
+    {
+        playerName = name;
+        haelth = initialHealth;
+        playerCount++;
+    }
+    public void Heal(int amount)
+    {
+        haelth = haelth + amount;
+        if (haelth > 100)
+        {
+            haelth = 100;
+        }
+        Debug.Log(haelth);
+    }
+    public void Heal(bool fullRestore)
+    {
+        if (fullRestore)
+        {
+            haelth = 100;
+        }
+    }
+    public static void ShowPlayerCount()
+    {
+        Debug.Log(playerCount);
+    }
 }
