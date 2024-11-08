1a09db3f44f2c990615e669d50119acb12deacfd
Mariam AL-batool <mariamalbatoolfarrah@gmail.com>
2024-11-08 11:18:37 +0200

    character class done

---
+++ Assets/Assignments/ِAssign.14/Character.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    private string name;
+    private int health;
+    public Character(string name, int health)
+    {
+        this.name = name;
+        this.health = health;
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public int Health
+    {
+        get { return health; }
+        set
+        {
+            health += value;
+            if (health + value > 100)
+                health = 100;
+        }
+
+    }
+}
