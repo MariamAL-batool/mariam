754d89871a5548f1cd51bf8fe2070c7789c85e4b
Mariam AL-batool <mariamalbatoolfarrah@gmail.com>
2024-12-12 18:46:52 +0200

    edited assignment 24

--- Assets/Assignments/ِِِAssign.24/RacingGame.cs
+++ Assets/Assignments/ِِِAssign.24/RacingGame.cs
@@ -7,31 +7,31 @@ public class RacingGame : MonoBehaviour
     public RaceState raceState;
     public void SimulateRace(RaceState raceState)
     {
-        switch(raceState)
+        switch (raceState)
         {
             case RaceState.Start:
-            Debug.Log("The race is about to begin. Get ready!");
-            break;
+                Debug.Log("The race is about to begin. Get ready!");
+                break;
             case RaceState.Accelerate:
-            Debug.Log("You press the gas pedal. The car speeds up!");
-            break;
+                Debug.Log("You press the gas pedal. The car speeds up!");
+                break;
             case RaceState.Turn:
-            Debug.Log("You approach a sharp turn. Be careful not to crash!");
-            break;
+                Debug.Log("You approach a sharp turn. Be careful not to crash!");
+                break;
             case RaceState.Crash:
-            Debug.Log("You hit a barrier. The race is over.");
-            break;
+                Debug.Log("You hit a barrier. The race is over.");
+                break;
             case RaceState.Finish:
-            Debug.Log("You crossed the finish line. Well done!");
-            break;
-            default :
-            Debug.Log("This state is not defined. Something is wrong.");
-            break;
+                Debug.Log("You crossed the finish line. Well done!");
+                break;
+            default:
+                Debug.Log("This state is not defined. Something is wrong.");
+                break;
         }
     }
 
     void Update()
     {
-      SimulateRace(raceState);
+        SimulateRace(raceState);
     }
 }
